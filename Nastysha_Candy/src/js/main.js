97eb8c0c59ce88aa687ef532d6ddad50e76732d1
Stepahaha <50381849+Stepahaha@users.noreply.github.com>
2020-03-18 11:13:26 +0300

    Add files via upload

--- Nastysha_Candy/src/js/main.js
+++ Nastysha_Candy/src/js/main.js
@@ -2,3 +2,29 @@ $('.header-menu-btn').click(function () {
  $('.header-menu').toggleClass('header-menu-active')
  $('.header-menu-btn').toggleClass('header-menu-btn-active');
 });
+
+var mySwiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 1,
+  allowTouchMove: true,
+  autoplay: {
+    delay: 7800,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+  breakpoints: {
+    550: {
+      slidesPerView: 2,
+      allowTouchMove: true,
+    },
+    780: {
+      slidesPerView: 3,
+      allowTouchMove: false,
+      autoplay: {
+        delay: 7800000,
+      },
+    }
+  }
+});
