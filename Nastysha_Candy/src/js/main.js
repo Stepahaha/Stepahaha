c534776f51d2b5b9ca018287afaafc07f6947163
Stepahaha <50381849+Stepahaha@users.noreply.github.com>
2020-04-01 17:42:09 +0300

    Add files via upload

--- Nastysha_Candy/src/js/main.js
+++ Nastysha_Candy/src/js/main.js
@@ -28,3 +28,64 @@ var mySwiper = new Swiper('.swiper-container', {
     }
   }
 });
+
+
+
+// const cardFront = document.querySelector('.front'),
+//       cardBack = document.querySelector('.back'),
+//       buttonFlip = document.querySelector('.flipBtn'),
+//       buttonFlipBack = document.querySelector('.catalog-goods-card-backFlip');
+
+
+
+
+// $(buttonFlip).click(function(){
+//  cardBack.style.transform = "rotateY(360deg)";
+//  cardFront.style.transform = "rotateY(180deg)";
+// });
+
+// $(buttonFlipBack).click(function(){
+//  cardBack.style.transform = "rotateY(180deg)";
+//  cardFront.style.transform = "rotateY(0deg)";
+// });
+
+
+
+
+// buttonFlip.addEventListener("click", function(){
+//   cardBack.style.transform = "rotateY(360deg)";
+//   cardFront.style.transform = "rotateY(180deg)";
+// });
+// buttonFlipBack.addEventListener("click", function(){
+//   cardBack.style.transform = "rotateY(180deg)";
+//   cardFront.style.transform = "rotateY(0deg)";
+// });
+
+
+
+
+
+
+
+
+const cards = document.querySelectorAll('.justCard');
+
+for (let i = 0; i < cards.length; i++) {
+ const card = cards[i];
+ card.addEventListener('mousemove', color);
+}
+
+function color(event) {
+ const cardFront = this.querySelector('.front'),
+       cardBack = this.querySelector('.back'),
+       flipBtn = this.querySelector('.flipBtn'),
+       flipBackBtn = this.querySelector('.catalog-goods-card-backFlip');
+ flipBtn.addEventListener('click', function() {
+  cardBack.style.transform = "rotateY(0deg)";
+  cardFront.style.transform = "rotateY(-180deg)";
+ });
+ flipBackBtn.addEventListener('click', function() {
+  cardBack.style.transform = "rotateY(180deg)";
+  cardFront.style.transform = "rotateY(0deg)";
+ });
+}
